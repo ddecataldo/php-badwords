dc5e35f4b441055976c1890d29d6bfa794e3e180
ddecataldo <davidedecataldo92@gmail.com>
2021-11-26 15:43:33 +0100

    Stampa testo e sostituzione caratteri

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    
+    <?php
+
+        $parolaUtente = $_GET["parola"];
+        $testoCensurato;
+
+
+        // Dichiato una variabile con del testo
+        $testo = "Questo è il mio primo paragrafo dichiarato in una variale php";
+        // Rimuovo gli spazi dalla variabile
+        $testoSenzaSpazi = trim($testo);
+        // Conto la lunghezza della stringa
+        $lunghezzaTesto = strlen($testoSenzaSpazi);
+
+        if($parolaUtente){
+            $testoCensurato = str_replace($parolaUtente, "***" , $testo);
+        }
+
+    ?>
+
+    <!-- Stampo a schermo il testo e la sua lunghezza -->
+    <p><strong>Testo:</strong> <?php echo $testoCensurato ?></p>
+    <p><strong>Lunghezza Testo:</strong> <?php echo $lunghezzaTesto ?></p>
+
+</body>
+</html>
